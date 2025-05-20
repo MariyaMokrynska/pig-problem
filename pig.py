c6d50b621664d57956ac206c02791ed39d1953e5
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-05-19 23:12:24 -0700

    done

--- pig.py
+++ pig.py
@@ -1,11 +1,27 @@
+import re
+
+
 def pig_latin(sentence):
-    # Write your solution here!
-    pass
+    words = sentence.split()
+    result = []
+    for word in words:
+        new_word = ''
+        # if word[0] != 'a' and word[0] != 'e' and word[0] != 'i' and word[0] != 'o' and word[0] != 'u':
+        if re.match('[^aeiou]', word[0]):
+            new_word = word[1:]
+            new_word += word[0]
+            new_word += 'ay'
+        else:
+            new_word = word
+        result.append(new_word)
+    return " ".join(result)
+
 
 # Test cases
 assert pig_latin("something") == "omethingsay"
 assert pig_latin("awesome") == "awesome"
-assert pig_latin("latin is a hard language") == "atinlay is a ardhay anguagelay"
+assert pig_latin(
+    "latin is a hard language") == "atinlay is a ardhay anguagelay"
 assert pig_latin("y") == "yay"
 assert pig_latin("e") == "e"
 
